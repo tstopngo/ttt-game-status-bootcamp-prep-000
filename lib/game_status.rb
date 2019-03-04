f27cf8839214846be0f335878978b4592472808d
Tracy Ngo <tngo1206@gmail.com>
2019-03-04 02:15:41 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -30,7 +30,7 @@ def draw?(board)
 end
 
 def over?(board)
-  if !!won?(board) || full?(board) || draw?(board)
+  if won?(board) || full?(board) || draw?(board)
     return true
   else
     return false
