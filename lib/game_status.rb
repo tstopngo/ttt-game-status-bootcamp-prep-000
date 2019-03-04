70a79d5096ac143cfe54e69b4b21d774c9a0050b
Tracy Ngo <tngo1206@gmail.com>
2019-03-04 00:02:43 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -12,7 +12,8 @@ def won?(board)
     if board[win_combo[0]] == board[win_combo[1]] &&
        board[win_combo[1]] == board[win_combo[2]] && position_taken?(board, win_combo[0]) 
       return win_combo
-
+          else
+       return false
     end
   end
  end
